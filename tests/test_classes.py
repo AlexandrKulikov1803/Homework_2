ac97ac47eb7993dc6556a64c367b9b1f135df741
Alexandr Kulikov <kulic1803@yandex.ru>
2025-07-22 16:15:28 +0600

    Adding tests for classes

---
+++ tests/test_classes.py
@@ -0,0 +1,23 @@
+from src.classes import Category, Product
+
+
+def test_product_init(product: Product) -> None:
+    assert product.name == "Samsung Galaxy S23 Ultra"
+    assert product.description == "256GB, Серый цвет, 200MP камера"
+    assert product.price == 180000.0
+    assert product.quantity == 5
+
+
+def test_category_init(first_category: Category, second_category: Category) -> None:
+    assert first_category.name == "Смартфоны"
+    assert (
+        first_category.description
+        == "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни"
+    )
+    assert len(first_category.products) == 3
+
+    assert first_category.category_count == 2
+    assert second_category.category_count == 2
+
+    assert first_category.product_count == 4
+    assert second_category.product_count == 4
