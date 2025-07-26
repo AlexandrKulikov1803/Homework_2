696c4949c3494c79581f159ed595ef01c701d502
Alexandr Kulikov <kulic1803@yandex.ru>
2025-07-27 02:25:09 +0600

    Adding tests for access modes, getters and setters

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -34,24 +34,24 @@ def test_create_objects_from_json(json_data: list[dict]) -> None:
         category_1.description
         == "Смартфоны, как средство не только коммуникации, но и получение дополнительных функций для удобства жизни"
     )
-    assert isinstance(category_1.products, list)
-    assert len(category_1.products) == 3
+    assert isinstance(category_1.products_list, list)
+    assert len(category_1.products_list) == 3
 
-    product_1 = category_1.products[0]
+    product_1 = category_1.products_list[0]
     assert isinstance(product_1, Product)
     assert product_1.name == "Samsung Galaxy C23 Ultra"
     assert product_1.description == "256GB, Серый цвет, 200MP камера"
     assert product_1.price == 180000.0
     assert product_1.quantity == 5
 
-    product_2 = category_1.products[1]
+    product_2 = category_1.products_list[1]
     assert isinstance(product_2, Product)
     assert product_2.name == "Iphone 15"
     assert product_2.description == "512GB, Gray space"
     assert product_2.price == 210000.0
     assert product_2.quantity == 8
 
-    product_3 = category_1.products[2]
+    product_3 = category_1.products_list[2]
     assert isinstance(product_3, Product)
     assert product_3.name == "Xiaomi Redmi Note 11"
     assert product_3.description == "1024GB, Синий"
@@ -65,10 +65,10 @@ def test_create_objects_from_json(json_data: list[dict]) -> None:
         category_2.description
         == "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником"
     )
-    assert isinstance(category_2.products, list)
-    assert len(category_2.products) == 1
+    assert isinstance(category_2.products_list, list)
+    assert len(category_2.products_list) == 1
 
-    product_4 = category_2.products[0]
+    product_4 = category_2.products_list[0]
     assert isinstance(product_4, Product)
     assert product_4.name == '55" QLED 4K'
     assert product_4.description == "Фоновая подсветка"
