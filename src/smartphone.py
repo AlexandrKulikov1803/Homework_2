8df4fe81d09e8d9ff64bd938de54db20a14ece8a
Alexandr Kulikov <kulic1803@yandex.ru>
2025-07-31 01:02:10 +0600

    Adding classes: Smartphone and LawnGrass

---
+++ src/smartphone.py
@@ -0,0 +1,28 @@
+from src.product import Product
+
+
+class Smartphone(Product):
+    """Класс для хранения продукта - смартфон"""
+
+    efficiency: str
+    model: str
+    memory: int
+    color: str
+
+    def __init__(
+        self, name, description, price, quantity, efficiency, model, memory, color
+    ):
+        """Конструктор для продукта - смартфон"""
+
+        super().__init__(name, description, price, quantity)
+        self.efficiency = efficiency
+        self.model = model
+        self.memory = memory
+        self.color = color
+
+    def __add__(self, other):
+        """Метод для подсчёта суммарной стоимости продуктов одного класса с учётом количества"""
+
+        if type(other) is Smartphone:
+            return self.price * self.quantity + other.price * other.quantity
+        raise TypeError
